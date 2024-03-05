c37e26929e02c134d8ff123d9f569ea2613aac53
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-03-05 22:22:41 +0300

    The whole project with a backup for the database

---
+++ DVLD Code and Database Backup/Code/DVLD_DataAccess/DataAccessSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=DVLD;User=sa;Password=1234";
+    }
+}
